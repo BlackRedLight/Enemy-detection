2b7bff35733533c9574ebb3836db3a6f076b07f9
BlackRedLight <blackredlight@BRL.Laptop>
2021-12-12 13:18:17 +0500

    Fixing qasciikey.cpp bug

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import matplotlib
 
 
 if __name__ == "__main__":
-	test_img = './Enemy images/Pigeon.jpg'
-
-	# img = cv2.imread(test_img)
-	# cv2.imshow('Enemy', img)
-	# cv2.waitKey(0)
-
+	
 	frameWidth = 640
 	frameHeight = 480
 	cap = cv2.VideoCapture(0)
-	cap.set(3, frameWidth)
-	cap.set(4, frameHeight)
-	cap.set(10, 150)
+	# cap.set(3, frameWidth)
+	# cap.set(4, frameHeight)
+	#cap.set(10, 150)  # brightness
 
 	while True:
 		success, img = cap.read()
-		cv2.imshow("Result", img)
-		c = cv2.waitKey(1)
 		
-		if c == 27:
+		if not success:
+			print("Can't receive frame (stream end?). Exiting ...")
 			break
+
+		cv2.imshow("Result", img)
+		
+		if cv2.waitKey(1) == ord('q'):
+			break
+
 	cap.release()
 	cv2.destroyAllWindows()
