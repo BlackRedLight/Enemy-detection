e8dbb80b1d5b078374f972f0aa843d89a4d70b48
BlackRedLight <blackredlight@BRL.Laptop>
2021-12-15 20:32:23 +0500

    Better background recognition

--- motion_cam.py
+++ motion_cam.py
@@ -4,6 +4,8 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 firstFrame = None
+i = 0
+background_found = False
 treshSensitivity = 15
 min_area = 5000
 
@@ -11,10 +13,9 @@ min_area = 5000
 while True:
 	# grab the current frame
 	success, frame = cap.read()
-	# let my webcam warmup and grab new frame
-	if firstFrame is None:
-		time.sleep(5.0)
-		success, frame = cap.read()
+
+	if background_found is False:
+		time.sleep(1.0)
 
 	if not success:
 		print('Can\'t read frames from the camera')
@@ -35,15 +36,33 @@ while True:
 	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 		cv2.CHAIN_APPROX_SIMPLE)[0]	
 	
+	
+
+	if (cnts == ()) and (background_found is False) and (i < 3):
+		i += 1
+		if i == 3:
+			background_found = True
+		print('Background')
+		continue
+	elif (cnts != ()) and (background_found is False):
+		firstFrame = gray
+		print('Working on it')
+		continue
+
+
 	for c in cnts:
 		# if the contour is too small, ignore it
 		if cv2.contourArea(c) >= min_area:
 			# compute the bounding box for the contour and draw it on the frame
 			(x, y, w, h) = cv2.boundingRect(c)
 			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
 	
-	cv2.imshow("Motion camera", frame)
-	if cv2.waitKey(250) == ord('q'):
+	# cv2.imshow('Motion camera', frame)
+	cv2.imshow('FUCK', thresh)
+	cv2.imshow('Background', firstFrame)
+	cv2.imshow('Motion', frame)
+	if cv2.waitKey(25) == ord('q'):
 		break
 	
 cap.release()
